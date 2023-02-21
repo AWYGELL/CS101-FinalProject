0d7bab890599cd95d7f6e5efc9683267b30e3243
ANGel <118653779+AWYGELL@users.noreply.github.com>
2023-02-21 11:54:40 +0800

    Update Startwithflask.py

--- Startwithflask.py
+++ Startwithflask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 import pymysql
 from flask import request
 
@@ -6,7 +6,9 @@ app = Flask(__name__, template_folder='html')
 
 @app.route("/first/step")
 def login():
-    return render_template("login.html")
+    if request.method == 'GET':
+        return render_template("login.html")
+    return redirect(url_for('Home_page'))
 
 @app.route("/register/step", methods=['GET', 'POST'])
 def register():
@@ -30,7 +32,7 @@ def register():
     cursor.close()
     conn.close()
 
-    return "Successfully registered!!!"
+    return redirect(url_for('Home_page'))
 
 @app.route("/post/step", methods=['POST'])
 def post_register():
@@ -39,50 +41,49 @@ def post_register():
     print(request.form)
     return render_template('Back_page.html')
 
-# @app.route('/Home/Page', methods=["POST","GET"])
-# def Home_page():
-#     message = ''
-#     if request.method == 'GET':
-#         return render_template('home.html', message='Please choose a button')
-#
-#     if request.method == 'POST':
-#         bt_a = request.values.get("button1")
-#         bt_b = request.values.get("button2")
-#         bt_c = request.values.get("button3")
-#         if (bt_a == 'Create a New Plan'):
-#             return render_template('CreateNewPlan.html')
-#         if (bt_b == 'View Plans'):
-#             return render_template('ViewPlans.html')
-#         if (bt_c == 'Search for Recommendations'):
-#             return render_template('SearchRecommendation.html')
+@app.route('/Home/Page', methods=["POST","GET"])
+def Home_page():
+    message = ''
+    if request.method == 'GET':
+        return render_template('home.html', message='Please choose a button')
+
+    if request.method == 'POST':
+        bt_a = request.values.get("button1")
+        bt_b = request.values.get("button2")
+        bt_c = request.values.get("button3")
+        if (bt_a == 'Create a New Plan'):
+            return render_template('CreatNewPlan.html')
+        if (bt_b == 'View Plans'):
+            return render_template('ViewPlans.html')
+        if (bt_c == 'Search for Recommendations'):
+            return render_template('SearchRecommendation.html')
 
 @app.route('/Plan/Page', methods=['GET','POST'])
-# 这里是要录入用户输入的所有信息，包括origin, destination, start date, return date, travel plan
-# 共有5个box，每一个box的datatype都是character，前四个box长度100个character，最后一个box长度能多长就多长
 def CreateNewPlan():
     if request.method == 'GET':
         return render_template("CreatNewPlan.html")
-
+    elif request.method == 'POST':
         origin = request.form.get("origin")
         destination = request.form.get("destination")
         startdate = request.form.get("startdate")
         returndate = request.form.get("returndate")
+        plan = request.form.get("plan")
 
         # connect to MYSQL
-        conn = pymysql.connect(host="127.0.0.1", port=3306, user="root", passwd="AWNYGELL030910", charset="utf8",
-                               db="db2")
+        conn = pymysql.connect(host="127.0.0.1", port=3306, user="root", passwd="AWNYGELL030910", charset="utf8", db="db2")
         cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
 
         # send command
-        sql = 'insert into register(origin,destination,startdate,returndate) values(%s,%s,%s,%s,%s)'
-        cursor.execute(sql, [origin,destination,startdate,returndate])
+        sql2 = 'insert into plan(origin,destination,startdate,returndate,plan) values(%s,%s,%s,%s,%s)'
+        cursor.execute(sql2, [origin,destination,startdate,returndate,plan])
         conn.commit()
 
         # close mysql
         cursor.close()
         conn.close()
 
-        return "Successfully planned!!!"
+        return redirect(url_for('Home_page'))
+
 
 # @app.route("SearchRecommendation", methods=['POST'])
 # # 这个页面会出现一个search box，让用户输入一个travel destination
